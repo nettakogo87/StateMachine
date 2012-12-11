6a352305e322a1e3258bf2d10a166e3bcb525a53
Vladimir <bratstakan@gmail.com>
2012-12-11 14:01:36 +0300

    Добавлен класс класса символов

---
+++ ClassOfSymbol.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+/**
+ * Класс класса символов. Нужен!
+ * Экземпляр может проверить пренадлежность символа классу.
+ */
+namespace StateMachine
+{
+    public class ClassOfSymbol
+    {
+        private string _name;
+        private char _startSymbol;
+        private char _endSymbol;
+
+        public string Name { get;set; }
+        public char StartSymbol { get; set; }
+        public char EndSymbol { get; set; }
+
+        public bool ContainsSymbol(string symbol)
+        {
+            string ss = _startSymbol.ToString();
+            string es = _endSymbol.ToString();
+            string pattern = ss == es ? @ss : @"[" + ss + "-" + es + "]";
+            RegexOptions option = RegexOptions.IgnoreCase;
+            Regex newReg = new Regex(pattern, option);
+            MatchCollection matches = newReg.Matches(symbol);
+            return matches.Count > 0;
+        }
+    }
+}
