c791f46e10605cf6ae99279154c42c816e8b99a9
Vladimir <bratstakan@gmail.com>
2012-12-13 18:43:28 +0300

    Добавлен класс лексемы

---
+++ Lexeme.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/**
+ * Класс лексемы.
+ */
+namespace StateMachine
+{
+    public class Lexeme
+    {
+        private string _name;
+        private int _countCharToReturn;
+        private int _finalState;
+
+        public string Name{ get; set; }
+        public int CountCharToReturn{ get; set; }
+        public int FinalState { get; set; }
+
+        public Lexeme(string name, int countCharToReturn, int finalState)
+        {
+            this._name = name;
+            this._countCharToReturn = countCharToReturn;
+            this._finalState = finalState;
+        }
+    }
+}
